9e9214b6cab9dc7f3427cf5606df1c0892e787bd
Leandro <leandro.almeida@uvvnet.com.br>
2024-06-11 11:26:32 -0300

    implementando o componente PublicarVaga

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -1,15 +1,60 @@
 <template>
-  <div>
-    <p>Publicar Vaga</p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais e de graça</h4>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Título da vaga</label>
+        <input type="text" class="form-control">
+        <div class="form-text">Por exemplo: Programador JavaScript e VueJS.</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Descrição</label>
+        <textarea type="text" class="form-control"></textarea>
+        <div class="form-text">Informe os detalhes da vaga.</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="" class="form-label">Salário</label>
+        <input type="number" class="form-control">
+        <div class="form-text">Informe o salário.</div>
+      </div>
+      <div class="col">
+        <label for="" class="form-label">Modalidade</label>
+        <select name="" id="" class="form-select">
+          <option value="1">Home Office</option>
+          <option value="1">Presencial</option>
+        </select>
+        <div class="form-text">Informe onde as atividades serão realizadas.</div>
+      </div>
+      <div class="col">
+        <label for="" class="form-label">Tipo</label>
+        <select name="" id="" class="form-select">
+          <option value="1">CLT</option>
+          <option value="1">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PublicarVaga'
-}
+  name: "PublicarVaga",
+};
 </script>
 
 <style>
-
 </style>
